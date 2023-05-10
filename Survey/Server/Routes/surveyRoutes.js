37d0d31aa0953ae98eb1ef52f039bb4a8167114f
brandonsussman <brandonsussman23@gmail.com>
2023-05-10 08:56:29 +0200

    endpoints to routes

--- Survey/Server/Routes/surveyRoutes.js
+++ Survey/Server/Routes/surveyRoutes.js
@@ -16,5 +16,88 @@ router.use(cors());
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
+router.get('/questionnaire', async (req, res) => {
+    try {
+      const questionnaire = await Questionnaire.findOne();
+  
+  
+      res.json(questionnaire);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
+  router.post('/surveys', (req, res, next) => {
+    const survey = req.body.answer;
+  
+    const answerArray = Object.entries(survey).map(([key, value]) => ({
+  
+      answer: value.value,
+      question: value.order
+    }));
+    answerArray.sort((a, b) => a.question - b.question);
+  
+    console.log('Client Token:', req.headers.authorization);
+    jwt.verify(req.headers.authorization, 'my_secret_key', (error, decoded) => {
+      if (error) {
+        console.log('Token:', req.headers.authorization);
+        console.log('Decoded Token:', decoded);
+        console.log('Error:', error);
+        res.status(401).send('Unauthorized.');
+  
+      } else {
+        console.log('Decoded Token:', decoded);
+        User.findById(decoded.userId)
+          .then((user) => {
+            if (!user) {
+              res.status(404).send('User not found.');
+            }
+  
+  
+            else {
+  
+              user.answer = answerArray;
+  
+  
+              console.log(user);
+  
+            }
+  
+            user.save()
+              .then((savedUser) => {
+                res.send('Survey completed successfully.');
+              })
+              .catch((error) => {
+                next(error);
+              });
+          })
+          .catch((error) => {
+            next(error);
+          });
+      }
+    });
+  });
+  
+  router.get('/surveys', (req, res, next) => {
+    const token = req.headers.authorization;
+    jwt.verify(token, 'my_secret_key', (error, decoded) => {
+      if (error) {
+        console.log('Error:', error);
+        res.status(401).send('Unauthorized.');
+      } else {
+        User.findById(decoded.userId)
+          .then((user) => {
+            if (!user) {
+              res.status(404).send('User not found.');
+            } else {
+              res.json(user.answer);
+            }
+          })
+          .catch((error) => {
+            next(error);
+          });
+      }
+    });
+  });
 
-//searchSurvey
+  module.exports=router;
---
+++ Survey/Server/Routes/userRoutes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10)
+    });
+    const savedUser = await user.save();
+    res.send('Registration successful.');
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.get('/check-login', (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).json({ message: 'Authorization header missing' });
+  } else {
+    jwt.verify(token, 'my_secret_key', (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid token' });
+      } else {
+        res.status(200).json({ message: 'Token is valid', decoded: decoded });
+      }
+    });
+  }
+});
+
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        res.status(401).send('Invalid email or password.');
+      }
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        res.status(401).send('Invalid email or password.');
+      }
+      const token = jwt.sign({ userId: user._id }, 'my_secret_key');
+      res.send({ token: token });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+
+
+module.exports = router;
