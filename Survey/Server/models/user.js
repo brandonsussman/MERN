f628a9c59ccffc92e65026f0894509fc26c818ab
brandonsussman <brandonsussman23@gmail.com>
2023-05-10 10:11:41 +0200

    changes to questionnaire survey

--- Survey/Server/models/user.js
+++ Survey/Server/models/user.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define survey schema
-const surveySchema = new mongoose.Schema({
-
-    
-      answer:{ type: String,
-        required: true},
-      question:{type: Number,
-        required: true}
-    
-    
-  }
-);
 
 // Define user schema with surveys subdocument
 const userSchema = new mongoose.Schema({
